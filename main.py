1353b29284d2a5ac8920014cba39e3f8d4820a04
Callum <callumjohn.allison@gmail.com>
2023-08-02 18:20:40 +0100

    Remove tesla-roulette project, add tesla image, modify resource reading for build

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import keyboard
 import tkinter as tk
-from importlib import resources
+from os import path
+from pathlib import Path
 from glob import glob
 from PIL import Image, ImageTk
 
 
 class ExileMemory:
-
     image_labels = [
         'Edanna Entry',
         'Voltaic Entry',
+        'Completed Tesla',
         'Amateria Entry',
         'Amateria Pegs',
         'Amateria Final 1',
@@ -26,8 +27,8 @@ class ExileMemory:
         self.index = 0
         self.photo_image = None
         self.root = tk.Tk()
-        self.images_path = resources.files('images')
-        self.images = sorted(glob(str(self.images_path) + '/*.jpg'))
+        self.images_path = path.join(Path(__file__).parent, 'images')
+        self.images = sorted(glob(self.images_path + '/*.jpg'))
         self.image_label = tk.Label(self.root)
         self.image_label.grid(row=0)
         self.text_label = tk.Label(self.root)
@@ -44,7 +45,7 @@ class ExileMemory:
                 self.forward()
 
     def set_image(self) -> None:
-        image = str(self.images_path.joinpath(self.images[self.index]))
+        image = self.images[self.index]
         open_image = Image.open(image)
         self.photo_image = ImageTk.PhotoImage(open_image)
         self.image_label.configure(image=self.photo_image)
@@ -69,4 +70,5 @@ class ExileMemory:
         self.set_image()
 
 
-ExileMemory()
+if __name__ == '__main__':
+    ExileMemory()
